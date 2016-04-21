4900402f16baad7f444ddc98200892792f65b138
kh90909 <kh90909@users.noreply.github.com>
2016-04-20 23:34:34 -0400

    Fix bug in update_variable()
    It was using the wrong selector to find the td element containing the
    variable value.

--- assets/js/app.js
+++ assets/js/app.js
@@ -240,7 +240,7 @@ $(function() {
       result = data.body.result.toString();
     }
 
-    $("[id='"+data.body.name+"']").html(result);
+    $("td[data-variable='"+data.body.name+"']").html(result);
   }
 
   function dump_variable(event){
